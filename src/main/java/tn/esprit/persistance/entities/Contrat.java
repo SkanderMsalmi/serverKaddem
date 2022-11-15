f817a1a2e1d58645c33e286529a14f4b8a6c801e
Skander <msalmi.skander@esprit.tn>
2022-11-15 17:19:17 +0100

    Partie1
    Créer les entités se trouvant dans le diagramme des classes

---
+++ src/main/java/tn/esprit/persistance/entities/Contrat.java
@@ -0,0 +1,46 @@
+package tn.esprit.persistance.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import tn.esprit.persistance.enumartion.Specialite;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="Contrat")
+public class Contrat implements Serializable{
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+@Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Integer idContrat;
+ @Temporal(TemporalType.DATE)
+ private Date dateDebutContrat;
+ @Temporal(TemporalType.DATE)
+ private Date dateFinContrat;
+ @Enumerated(EnumType.STRING)
+ private Specialite specialite;
+ private boolean archive;
+ private Integer montantContrat;
+}
---
+++ src/main/java/tn/esprit/persistance/entities/DetailEquipe.java
@@ -0,0 +1,36 @@
+package tn.esprit.persistance.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="DetailEquipe")
+public class DetailEquipe implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="idDetailEquipe")
+	private Integer idDetailEquipe;
+	private Integer salle;
+	private String thematique;
+}
